388d45a54505ae11f21dad5d4a0aa22305715db3
shkkaise <123665233+shkkaise@users.noreply.github.com>
2024-12-06 10:31:01 +0000

    Repl local with capybara

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://humble-space-capybara-69vv9g7474qf5j6v-4000.app.github.dev:4000';
 export const currency = '₹';
 
 function App() {
